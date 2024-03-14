6bd9e3d377f4c5f1c4621b3ac77de9babe2f3969
ProfesorGaz <d.juan.gaz@cesba-queretaro.edu.mx>
2024-03-14 08:03:41 -0600

    1º commiit

---
+++ src/mx/edu/cesba/Matematicas.java
@@ -0,0 +1,45 @@
+package mx.edu.cesba;
+
+public class Matematicas {
+
+    public static void main(String[] args) {
+
+    }
+// REALIZAR UN PROGRAMA CON SW QUE PUEADA REALIZAR UNA DE LAS 9 FUNCIONES
+    // que solicite el usario
+
+    /* 
+    double numero = 25;
+        double resultado = Math.sqrt(numero);
+        System.out.println
+        ("La raíz cuadrada del número " + 
+                numero + " es: " + resultado);
+    
+    
+     
+    double base = 5;
+        double exponente = 3;
+        double resultado = Math.pow(base, exponente);
+        System.out.println("El número " + base + 
+                " elevado a la potencia " + exponente + " es: " 
+                + resultado);
+    
+    double numero = 5.8;
+        long resultado = Math.round(numero);
+        System.out.println("Redondeando al entero más cercano el número "
+                + numero + ": " + resultado);  double numero = 5.7;
+        double resultado = Math.floor(numero);
+        System.out.println("Redondeando hacia abajo el número "
+                + numero + ": " + resultado);
+       double numero = 5.3;
+        double resultado = Math.ceil(numero);
+        System.out.println
+        ("Redondeando hacia arriba el número " + numero + ": " + resultado)
+    
+    double radio = 5.0;                 // valor de radio al Cuadraro
+        double area = Math.PI * Math.pow(radio, 2);
+        System.out.println
+        ("El área del círculo con radio " + radio + " es: " + area);
+        
+        System.out.println(Math.PI);*/
+}
